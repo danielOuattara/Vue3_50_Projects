8df8e5fbfad0b3b1c1cd3807bedc8ad08ab135ce
danielOuattara <daniel.ouattara@gmx.com>
2023-02-16 13:04:01 +0100

    starting  app project 06

---
+++ 06-vue3-portfolio-jobs-experience-tabs-app/src/Index.vue
@@ -0,0 +1,17 @@
+<template>
+  <AppOptionsAPI />
+  <AppCompositionAPI />
+</template>
+
+<script>
+import AppOptionsAPI from "./01_vue3_options_api/AppOptionsAPI.vue";
+import AppCompositionAPI from "./02_vue3_composition_api/AppCompositionAPI.vue";
+
+export default {
+  name: "Index",
+  components: {
+    AppOptionsAPI,
+    AppCompositionAPI,
+  },
+};
+</script>
