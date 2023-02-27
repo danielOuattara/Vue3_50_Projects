b3d0414a3424372a40735b1d04161164709f4285
danielOuattara <daniel.ouattara@gmx.com>
2023-02-27 19:57:43 +0100

    options API solution OK

---
+++ 08-vue3-lorem-ipsum/src/_01_vue3_options_api/AppOptionsAPI.vue
@@ -0,0 +1,44 @@
+<template>
+  <section class="section-center">
+    <h3>tired of boring lorem ipsum ?</h3>
+
+    <form class="lorem-form" @submit.prevent="handleSubmit">
+      <label for="numbParagraph">number of paragraph ?</label>
+      <input
+        id="numbParagraph"
+        type="number"
+        min="1"
+        :max="data.length"
+        v-model="numberOfParagraph"
+      />
+      <button type="submit" class="btn">Generate</button>
+    </form>
+
+    <article class="lorem-text">
+      <p v-for="(textItem, index) in text" :key="index">{{ textItem }}</p>
+    </article>
+  </section>
+</template>
+
+<script>
+import data from "./../data";
+export default {
+  name: "App",
+  components: {},
+  data() {
+    return {
+      data,
+      numberOfParagraph: 1,
+      text: [],
+    };
+  },
+
+  methods: {
+    handleSubmit() {
+      return (this.text = this.data.slice(0, parseInt(this.numberOfParagraph)));
+    },
+  },
+};
+</script>
+
+<style></style>
