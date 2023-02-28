ef648f328f62b1f82fca372773eda70c3f599b10
danielOuattara <daniel.ouattara@gmx.com>
2023-02-28 23:23:28 +0100

    renamed project folder

---
+++ 01-vue3-birthday-reminder-app/src/Index.vue
@@ -0,0 +1,31 @@
+<template>
+  <AppOptionsAPI />
+  <br />
+  <hr />
+  <br />
+  <AppCompositionAPI />
+</template>
+
+<script>
+import AppOptionsAPI from "./_01_vue3_options_api/AppOptionsAPI.vue";
+import AppCompositionAPI from "./_02_vue3_composition_api/AppCompositionAPI.vue";
+
+export default {
+  name: "Index",
+  components: {
+    AppOptionsAPI,
+    AppCompositionAPI,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
