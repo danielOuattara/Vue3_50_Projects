2985593d6a8b1c87cbd316a8837df2913516de5b
danielOuattara <daniel.ouattara@gmx.com>
2023-03-10 23:15:07 +0100

    updated index.vue

--- 10-vue3-grocery-bud-app/src/Index.vue
+++ 10-vue3-grocery-bud-app/src/Index.vue
@@ -3,15 +3,30 @@
   <br />
   <hr />
   <br />
-  <AppCompositionAPI />
+  <AppCompositionAPI_V1 />
+  <br />
+  <hr />
+  <br />
+  <AppCompositionAPI_V2 />
+  <br />
+  <hr />
+  <br />
+  <AppCompositionAPI_V3 />
 </template>
 
 <script>
 import AppOptionsAPI from "./_01_vue3_options_api/AppOptionsAPI.vue";
-import AppCompositionAPI from "./_02_vue3_composition_api/AppCompositionAPI.vue";
+import AppCompositionAPI_V1 from "./_02_vue3_composition_api_v1/AppCompositionAPI_V1.vue";
+import AppCompositionAPI_V2 from "./_02_vue3_composition_api_v2/AppCompositionAPI_V2.vue";
+import AppCompositionAPI_V3 from "./_02_vue3_composition_api_v3/AppCompositionAPI_V3.vue";
 export default {
   name: "Index",
-  components: { AppOptionsAPI, AppCompositionAPI },
+  components: {
+    AppOptionsAPI,
+    AppCompositionAPI_V1,
+    AppCompositionAPI_V2,
+    AppCompositionAPI_V3,
+  },
   setup() {
     return {};
   },
