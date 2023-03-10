884c331769abd5c0a2beba2779ff20573da5bc5d
danielOuattara <daniel.ouattara@gmx.com>
2023-03-10 15:56:00 +0100

    10 grocery bud: solution  for options API

--- 10-vue3-grocery-bud-app/src/_01_vue3_options_api/components/Alert.vue
+++ 10-vue3-grocery-bud-app/src/_01_vue3_options_api/components/Alert.vue
@@ -1,9 +1,45 @@
 <template>
-  <div></div>
+  <p :class="`alert alert-${alert.type}`">{{ alert.message }}</p>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "Alert",
+  props: ["alert", "showAlert"],
+
+  // mounted() {
+  //   // console.log(this.alert);
+  //   // console.log(this.alert.message);
+  //   clearTimeout(this.timeoutID);
+  // },
+
+  data() {
+    return {
+      timeoutID: undefined,
+    };
+  },
+
+  // watch: {
+  //   alert(newAlert, oldAlert) {
+  //     if (newAlert !== oldAlert) {
+  //       console.log("in watch");
+  //       this.timeoutID = setTimeout(() => {
+  //         return this.showAlert();
+  //       }, 1500);
+  //     }
+  //   },
+  // },
+
+  updated() {
+    this.timeoutID = setTimeout(() => {
+      return this.showAlert();
+    }, 1500);
+  },
+
+  beforeMount() {
+    clearTimeout(this.timeoutID);
+  },
+};
 </script>
 
 <style lang="scss" scoped></style>
