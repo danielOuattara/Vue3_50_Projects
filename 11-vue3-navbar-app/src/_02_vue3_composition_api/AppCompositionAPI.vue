0c35223da688743466a41f190304e064ec818289
danielOuattara <daniel.ouattara@gmx.com>
2023-03-13 23:58:44 +0100

    11 navbar composition API solution

---
+++ 11-vue3-navbar-app/src/_02_vue3_composition_api/AppCompositionAPI.vue
@@ -0,0 +1,16 @@
+<template>
+  <b><p :style="{ marginLeft: '5rem' }">composition API solution</p></b>
+  <Navbar />
+</template>
+
+<script>
+import Navbar from "./components/Navbar.vue";
+
+export default {
+  components: {
+    Navbar,
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
