3b309e25fb60f456c88c7982d23253a78f161ff1
danielOuattara <daniel.ouattara@gmx.com>
2023-03-22 14:53:48 +0100

    12:optionAPI provide/inject, reactivity problem DO

---
+++ 12-vue3-sidebar-modal-app/src/_03_vue3_options_api_provide_inject/AppOptionsAPIProvideInject.vue
@@ -0,0 +1,48 @@
+<template>
+  <br />
+  <hr />
+  <br />
+  <p style="text-align: center">options API provide/inject solution !</p>
+  <Home @onToggleSideBar="toggleSideBar" @onToggleModal="toggleModal" />
+  <Modal @onToggleModal="toggleModal" />
+  <SideBar @onToggleSideBar="toggleSideBar" />
+</template>
+
+<script>
+import Home from "./components/Home.vue";
+import Modal from "./components/Modal.vue";
+import SideBar from "./components/SideBar.vue";
+import { computed } from "vue";
+
+export default {
+  name: "AppOptionsAPI",
+  components: {
+    Home,
+    Modal,
+    SideBar,
+  },
+
+  data() {
+    return {
+      isSideBarOpen: false,
+      isModalOpen: false,
+    };
+  },
+
+  provide() {
+    return {
+      isModalOpen: computed(() => this.isModalOpen),
+      isSideBarOpen: computed(() => this.isSideBarOpen),
+    };
+  },
+
+  methods: {
+    toggleSideBar() {
+      this.isSideBarOpen = !this.isSideBarOpen;
+    },
+    toggleModal() {
+      this.isModalOpen = !this.isModalOpen;
+    },
+  },
+};
+</script>
---
+++ 12-vue3-sidebar-modal-app/src/_03_vue3_options_api_provide_inject/components/Modal.vue
@@ -0,0 +1,19 @@
+<template>
+  <div :class="isModalOpen ? `modal-overlay show-modal` : `modal-overlay`">
+    <div class="modal-container">
+      <h3>modal content</h3>
+      <p>function solution</p>
+      <button className="close-modal-btn" @click="$emit('onToggleModal')">
+        <font-awesome-icon icon="fa-solid fa-times" />
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Modal",
+  inject: ["isModalOpen"],
+  emits: ["onToggleModal"],
+};
+</script>
