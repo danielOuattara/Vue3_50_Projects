1f541ba0c00b0668f7495087c3fde2a429819b97
danielOuattara <daniel.ouattara@gmx.com>
2023-03-22 12:15:25 +0100

    12: composition API solution

---
+++ 12-vue3-sidebar-modal-app/src/_02_vue3_composition_api/AppCompositionAPI.vue
@@ -0,0 +1,40 @@
+<template>
+  <p style="text-align: center">composition API solution !</p>
+  <Home @onToggleSideBar="toggleSideBar" @onToggleModal="toggleModal" />
+  <Modal :isModalOpen="isModalOpen" @onToggleModal="toggleModal" />
+  <SideBar :isSideBarOpen="isSideBarOpen" @onToggleSideBar="toggleSideBar" />
+</template>
+
+<script>
+import Home from "./components/Home.vue";
+import Modal from "./components/Modal.vue";
+import SideBar from "./components/SideBar.vue";
+import { ref } from "vue";
+
+export default {
+  name: "AppOptionsAPI",
+  components: {
+    Home,
+    Modal,
+    SideBar,
+  },
+  setup() {
+    const isSideBarOpen = ref(false);
+    const isModalOpen = ref(false);
+    function toggleSideBar() {
+      isSideBarOpen.value = !isSideBarOpen.value;
+    }
+
+    function toggleModal() {
+      isModalOpen.value = !isModalOpen.value;
+    }
+
+    return {
+      isSideBarOpen,
+      isModalOpen,
+      toggleSideBar,
+      toggleModal,
+    };
+  },
+};
+</script>
---
+++ 12-vue3-sidebar-modal-app/src/_02_vue3_composition_api/components/SideBar.vue
@@ -0,0 +1,47 @@
+<template>
+  <aside :class="isSideBarOpen ? 'sidebar show-sidebar' : 'sidebar'">
+    <div class="sidebar-header">
+      <img :src="logoIcon" alt="logo" class="logo" />
+      <button class="close-btn" @click="$emit('onToggleSideBar')">
+        <font-awesome-icon icon="fa-solid fa-times" />
+      </button>
+    </div>
+    <ul class="links">
+      <li v-for="item in linksIcons" :key="item.id">
+        <a :href="item.url"
+          ><font-awesome-icon :icon="item.icon" /> {{ item.text }}</a
+        >
+      </li>
+    </ul>
+
+    <ul class="social-icons">
+      <li v-for="item in socialsIcons" :key="item.id">
+        <a :href="item.url"><font-awesome-icon :icon="item.icon" /></a>
+      </li>
+    </ul>
+  </aside>
+</template>
+
+<script>
+import logo from "./../../logo.svg";
+import { socials, links } from "./../../data";
+import { ref } from "vue";
+export default {
+  name: "SideBar",
+  props: ["isSideBarOpen"],
+  emits: ["onToggleSideBar"],
+
+  setup() {
+    const logoIcon = ref(logo);
+    const socialsIcons = ref(socials);
+    const linksIcons = ref(links);
+    return {
+      logoIcon,
+      socialsIcons,
+      linksIcons,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
